69d33493fec693e7046095cad839e9790cac3fbe
kevin.d.tyo@gmail.com <kevin.d.tyo@gmail.com>
2018-08-31 17:48:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musician instrument){
+  var strings = []
+  for (i = 0; i > 10i++);
+  if (i === 0)
+    array.push("${musicians}""plays""${instrument}");
+    return theBeatlesPlay
+}
